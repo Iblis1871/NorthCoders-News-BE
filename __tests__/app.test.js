94de11785d29fd9fabd95e978c9f8d97c510730e
iblis <chris.strafford@gmail.com>
2022-03-30 12:02:04 +0100

    nc-news-5 fourth ticket third review, with forEach get/users test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -84,58 +84,73 @@ describe("GET api/users", () => {
         });
       });
   });
-});
+  test("status:200, responds an array of users with usernames FOR EACH", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then((response) => {
+        response.body.users.forEach((object) => {
+          expect(object).toEqual(
+            expect.objectContaining({
+              username: expect.any(String),
+              name: expect.any(String),
+              avatar_url: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+  ///////////////////////////ERRORS////////////////////////////////////
 
-///////////////////////////////////////////////////////////////
-
-describe("ERROR TESTING ARTICLES", () => {
-  test("status:404, path not found", () => {
-    return request(app)
-      .get("/api/top")
-      .expect(404)
-      .then((res) => {
-        console.log(res.body);
-        expect(res.body.msg).toBe("path not found!");
-      });
+  describe("ERROR TESTING ARTICLES", () => {
+    test("status:404, path not found", () => {
+      return request(app)
+        .get("/api/top")
+        .expect(404)
+        .then((res) => {
+          console.log(res.body);
+          expect(res.body.msg).toBe("path not found!");
+        });
+    });
+    test("status:400, bad request", () => {
+      return request(app)
+        .get("/api/articles/99l99")
+        .expect(400)
+        .then((res) => {
+          console.log(res.body);
+          expect(res.body.msg).toBe("bad request!");
+        });
+    });
+    test("status:404, responds with a path not found ID DOES NOT EXIST", () => {
+      return request(app)
+        .patch("/api/articles/999")
+        .send({ inc_votes: 1 })
+        .expect(404)
+        .then((res) => {
+          expect(res.statusCode).toBe(404);
+          expect(res.body.msg).toBe("path not found!");
+        });
+    });
+    test("status:400, responds with an invalid invalid path", () => {
+      return request(app)
+        .patch("/api/articles/someNonsense")
+        .send({ inc_votes: 1 })
+        .expect(400)
+        .then((res) => {
+          expect(res.statusCode).toBe(400);
+          expect(res.body.msg).toBe("bad request!");
+        });
+    });
   });
-  test("status:400, bad request", () => {
-    return request(app)
-      .get("/api/articles/99l99")
-      .expect(400)
-      .then((res) => {
-        console.log(res.body);
-        expect(res.body.msg).toBe("bad request!");
-      });
-  });
-  test("status:404, responds with a path not found ID DOES NOT EXIST", () => {
-    return request(app)
-      .patch("/api/articles/999")
-      .send({ inc_votes: 1 })
-      .expect(404)
-      .then((res) => {
-        expect(res.statusCode).toBe(404);
-        expect(res.body.msg).toBe("path not found!");
-      });
-  });
-  test("status:400, responds with an invalid invalid path", () => {
-    return request(app)
-      .patch("/api/articles/someNonsense")
-      .send({ inc_votes: 1 })
-      .expect(400)
-      .then((res) => {
-        expect(res.statusCode).toBe(400);
-        expect(res.body.msg).toBe("bad request!");
-      });
-  });
-});
-describe("ERROR TESTING USERS", () => {
-  test("status:404, path not found", () => {
-    return request(app)
-      .get("/api/use")
-      .expect(404)
-      .then((res) => {
-        console.log(res.body);
-        expect(res.body.msg).toBe("path not found!");
-      });
+  describe("ERROR TESTING USERS", () => {
+    test("status:404, path not found", () => {
+      return request(app)
+        .get("/api/use")
+        .expect(404)
+        .then((res) => {
+          console.log(res.body);
+          expect(res.body.msg).toBe("path not found!");
+        });
+    });
   });
 });
