7150ee885c44cdf55bc5de7a9e1043cd3ff8df50
iblis <chris.strafford@gmail.com>
2022-03-30 11:38:16 +0100

    nc-news-5 fourth ticket second review, made a mess of order of branching but actioned all suggestions from last review

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -85,3 +85,57 @@ describe("GET api/users", () => {
       });
   });
 });
+
+///////////////////////////////////////////////////////////////
+
+describe("ERROR TESTING ARTICLES", () => {
+  test("status:404, path not found", () => {
+    return request(app)
+      .get("/api/top")
+      .expect(404)
+      .then((res) => {
+        console.log(res.body);
+        expect(res.body.msg).toBe("path not found!");
+      });
+  });
+  test("status:400, bad request", () => {
+    return request(app)
+      .get("/api/articles/99l99")
+      .expect(400)
+      .then((res) => {
+        console.log(res.body);
+        expect(res.body.msg).toBe("bad request!");
+      });
+  });
+  test("status:404, responds with a path not found ID DOES NOT EXIST", () => {
+    return request(app)
+      .patch("/api/articles/999")
+      .send({ inc_votes: 1 })
+      .expect(404)
+      .then((res) => {
+        expect(res.statusCode).toBe(404);
+        expect(res.body.msg).toBe("path not found!");
+      });
+  });
+  test("status:400, responds with an invalid invalid path", () => {
+    return request(app)
+      .patch("/api/articles/someNonsense")
+      .send({ inc_votes: 1 })
+      .expect(400)
+      .then((res) => {
+        expect(res.statusCode).toBe(400);
+        expect(res.body.msg).toBe("bad request!");
+      });
+  });
+});
+describe("ERROR TESTING USERS", () => {
+  test("status:404, path not found", () => {
+    return request(app)
+      .get("/api/use")
+      .expect(404)
+      .then((res) => {
+        console.log(res.body);
+        expect(res.body.msg).toBe("path not found!");
+      });
+  });
+});
